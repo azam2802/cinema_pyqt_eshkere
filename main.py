495394445f5a7390e3170f7a3bf8cbc60093be15
azam2802 <azamumarov182@gmail.com>
2024-12-03 19:43:27 +0600

    add: register page, flask code

--- main.py
+++ main.py
@@ -1,9 +1,94 @@
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QSpacerItem, QSizePolicy
+    QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QCheckBox, QSpacerItem, QSizePolicy
 )
 from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtCore import Qt
-from PyQt5.QtWebEngineWidgets import QWebEngineView  # Для отображения reCAPTCHA
+import sys
+
+
+class RegistrationPage(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Настройка окна
+        self.setWindowTitle("Регистрация")
+        self.setFixedSize(800, 600)
+
+        # Фоновое изображение
+        self.background_label = QLabel(self)
+        self.background_label.setPixmap(QPixmap("./img/back.jpg").scaled(
+            self.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation
+        ))
+        self.background_label.setGeometry(0, 0, 800, 600)
+
+        # Полупрозрачный затемняющий слой
+        self.overlay = QLabel(self)
+        self.overlay.setGeometry(0, 0, 800, 600)
+        self.overlay.setStyleSheet("background-color: rgba(0, 0, 0, 0.5);")
+
+        # Текст "Регистрация"
+        self.title_label = QLabel("Регистрация", self)
+        self.title_label.setFont(QFont("Arial", 42))
+        self.title_label.setStyleSheet("color: white;")
+        self.title_label.setAlignment(Qt.AlignCenter)
+
+        # Поля ввода
+        self.username_input = QLineEdit(self)
+        self.username_input.setPlaceholderText("Имя пользователя")
+        self.username_input.setFixedSize(295, 40)
+        self.username_input.setAlignment(Qt.AlignCenter)
+        self.username_input.setStyleSheet("border-radius: 5px;")
+
+        self.password_input = QLineEdit(self)
+        self.password_input.setPlaceholderText("Пароль")
+        self.password_input.setEchoMode(QLineEdit.Password)
+        self.password_input.setFixedSize(295, 40)
+        self.password_input.setAlignment(Qt.AlignCenter)
+        self.password_input.setStyleSheet("border-radius: 5px;")
+
+        # Кнопка регистрации
+        self.register_button = QPushButton("Зарегистрироваться", self)
+        self.register_button.setFixedSize(295, 40)
+        self.register_button.setStyleSheet(
+            "background-color: #E50914; color: white; font-size: 16px; border: none; border-radius: 5px;"
+        )
+
+        # Капча (ссылка на реCAPTCHA, заменить на реальную капчу)
+        self.recaptcha_checkbox = QCheckBox("I'm not a robot", self)
+        self.recaptcha_checkbox.setStyleSheet("color: white;")
+
+        # Кнопка назад
+        self.back_button = QPushButton("Назад", self)
+        self.back_button.setFixedSize(295, 40)
+        self.back_button.setStyleSheet(
+            "background-color: #E50914; color: white; font-size: 16px; border: none; border-radius: 5px;"
+        )
+        self.back_button.clicked.connect(self.go_back_to_login)
+
+        # Компоновка элементов
+        form_layout = QVBoxLayout()
+        form_layout.setAlignment(Qt.AlignCenter)
+        form_layout.addWidget(self.title_label)
+        form_layout.addSpacing(20)
+        form_layout.addWidget(self.username_input)
+        form_layout.addWidget(self.password_input)
+        form_layout.addWidget(self.register_button)
+        form_layout.addWidget(self.recaptcha_checkbox)
+        form_layout.addWidget(self.back_button)
+
+        # Основной компоновщик для центрирования
+        main_layout = QVBoxLayout(self)
+        main_layout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding))  # Верхний отступ
+        main_layout.addLayout(form_layout)  # Центрируемая форма
+        main_layout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding))  # Нижний отступ
+
+
+    def go_back_to_login(self):
+        # Создаем и показываем окно авторизации
+        self.login_page = LoginPage()
+        self.login_page.show()
+        self.close()
+
 
 class LoginPage(QWidget):
     def __init__(self):
@@ -52,30 +137,14 @@ class LoginPage(QWidget):
             "background-color: #E50914; color: white; font-size: 16px; border: none; border-radius: 5px;"
         )
 
-        # Виджет для отображения Google reCAPTCHA
-        self.recaptcha_view = QWebEngineView(self)
-        self.recaptcha_view.setFixedSize(320, 100)
-        self.recaptcha_view.setHtml("""
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <title>reCAPTCHA</title>
-                <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-            </head>
-            <body>
-                <form action="submit.html" method="POST">
-                    <div class="g-recaptcha" data-sitekey="6LeyLJEqAAAAAFG_tm9Sl8aj1C_WIRmz-kcp0_mG"></div>
-                </form>
-            </body>
-            </html>
-        """)
+        # Чекбокс reCAPTCHA
+        self.recaptcha_checkbox = QCheckBox("I'm not a robot", self)
+        self.recaptcha_checkbox.setStyleSheet("color: white;")
 
         # Ссылка регистрации
         self.register_link = QLabel('<a href="#">Регистрация</a>', self)
         self.register_link.setStyleSheet("color: lightblue;")
         self.register_link.setAlignment(Qt.AlignCenter)
-        self.register_link.setOpenExternalLinks(True)
 
         # Приветственное сообщение
         self.welcome_label = QLabel("Добро пожаловать в Eshkere movies!", self)
@@ -91,7 +160,7 @@ class LoginPage(QWidget):
         form_layout.addWidget(self.username_input)
         form_layout.addWidget(self.password_input)
         form_layout.addWidget(self.login_button)
-        form_layout.addWidget(self.recaptcha_view, alignment=Qt.AlignCenter)  # reCAPTCHA виджет
+        form_layout.addWidget(self.recaptcha_checkbox, alignment=Qt.AlignCenter)
         form_layout.addWidget(self.register_link)
 
         # Основной компоновщик для центрирования
@@ -106,14 +175,23 @@ class LoginPage(QWidget):
         self.username_input.raise_()
         self.password_input.raise_()
         self.login_button.raise_()
-        self.recaptcha_view.raise_()
+        self.recaptcha_checkbox.raise_()
         self.register_link.raise_()
         self.welcome_label.raise_()
 
+        # Сигнал для открытия окна регистрации
+        self.register_link.linkActivated.connect(self.open_registration_page)
+
+    def open_registration_page(self):
+        # Создаем и показываем окно регистрации
+        self.registration_page = RegistrationPage()
+        self.registration_page.show()
+        self.close()
+
 
 if __name__ == "__main__":
-    import sys
     app = QApplication(sys.argv)
     window = LoginPage()
     window.show()
     sys.exit(app.exec_())
+
